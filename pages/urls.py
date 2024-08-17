b3d93cae7e6b73c0423ce76073d6e3d287e4a5b8
RezaBR <rezamanzari80@gmail.com>
2024-08-17 21:57:07 +0330

    added home & aboutus page

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+urlpatterns =[
+    path('admin/', admin.site.urls), 
+    path('',views.HomePageView.as_view(), name= 'home'),
+    path('aboutus', views.AboutusPageView.as_view(), name= 'aboutus')
+    ]
