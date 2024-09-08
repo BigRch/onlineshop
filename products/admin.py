39e9da2af7dd865abd1f90254e2d889400ae07c6
RezaBR <rezamanzari80@gmail.com>
2024-09-08 12:45:34 +0330

    added a comment feature

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Comment
+
+class CommentsInline(admin.StackedInline):
+    model = Comment
+    fields = ['author', 'body', 'points', 'active', ]
+    extra = 0
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','price','active']
+    list_display = ['title', 'price', 'active',]
+    inlines = [CommentsInline,]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'date_created', 'points', 'active', ]
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
 # Create your models here.
 
@@ -14,4 +15,29 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("product_detail", args=[self.pk])
+    
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+
+    PRODUCT_POINT = [
+        ('1', 'Very Bad'),
+        ('2', 'Bad'),
+        ('3', 'Normal'),
+        ('4', 'Good'),
+        ('5', 'Perfect'),
+    ]
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    points = models.CharField(max_length=10, choices=PRODUCT_POINT)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    active = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse("product_detail", args= [self.product.id])
     
