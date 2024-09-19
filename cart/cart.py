b6c6333d1957b543b6a19604aed823293d345799
RezaBR <rezamanzari80@gmail.com>
2024-09-19 23:23:10 +0330

    Implemented Cart Functionality

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.utils.translation import gettext as _
 from products.models import Product
 
 class Cart:
@@ -12,21 +14,27 @@ class Cart:
             
         self.cart = cart
 
-    def add(self, product, quantity=1):
+    def add(self, product, quantity=1, replace_quantity = False):
         """add the specified Product if exists"""
         product_id = str(product.id)
 
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': quantity}
+            self.cart[product_id] = {'quantity': 0}
+
+        if replace_quantity:
+            self.cart[product_id]['quantity'] = quantity
+
         else:
             self.cart[product_id]['quantity'] += quantity
         self.save()
+        messages.success(self.request, _("Added to your cart"))
 
     def remove(self, product):
         """remove product from cart"""
         product_id = str(product.id)
         if product_id in self.cart:
-            del self.cart['product_id']
+            del self.cart[product_id]
+            messages.success(self.request, _("Product removed from your cart"))
             self.save()
 
     def save(self):
@@ -42,10 +50,11 @@ class Cart:
             cart[str(product.id)]['product_obj'] = product
         
         for item in cart.values():
+            item['total_price'] = item['product_obj'].price * item['quantity']
             yield item
 
     def __len__(self):
-        return len(self.cart.keys())
+        return sum(item['quantity'] for item in self.cart.values())
 
     def clear(self):
         del self.session['cart']
@@ -55,4 +64,4 @@ class Cart:
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
 
-        return sum([product.price for product in products])
+        return sum([item['quantity'] * item['product_obj'].price for item in self.cart.values()])
