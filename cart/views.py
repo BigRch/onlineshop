beacf9495745d26656d915f5a18613fac70ad717
RezaBR <rezamanzari80@gmail.com>
2024-10-08 21:48:25 +0330

    added clear cart functionality and some small fixes

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.utils.translation import gettext as _
 from .cart import Cart
 from products.models import Product
 from .forms import AddToCartProductForm
@@ -27,12 +29,22 @@ def add_to_cart_view(request, product_id):
         cleand_data = form.cleaned_data
         quantity = cleand_data['quantity']
         cart.add(product, quantity, replace_quantity=cleand_data['inpalce'],)
-    
-    return redirect('cart:cart_detail')
+    return redirect('product_list')
 
 def remove_from_cart(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
 
+    return redirect('cart:cart_detail')
+
+def clear_cart(request):
+    cart = Cart(request)
+
+    if len(cart):
+        cart.clear()
+        messages.success(request, _("Your cart has cleared successfully"))
+    else:
+        messages.success(request, _("Your cart is empty already"))
+    
     return redirect('cart:cart_detail')
