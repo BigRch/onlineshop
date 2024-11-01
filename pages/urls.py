bc759b6e01b4532967eadca9803a164c6174f084
RezaBR <rezamanzari80@gmail.com>
2024-11-01 18:05:03 +0330

    added wishlist page and updated comment section

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns =[
     path('admin/', admin.site.urls), 
     path('',views.HomePageView.as_view(), name= 'home'),
-    path('aboutus', views.AboutusPageView.as_view(), name= 'aboutus')
+    path('aboutus', views.AboutusPageView.as_view(), name= 'aboutus'),
+    path ('wishlist/', views.WishListView.as_view(), name='wishlist')
     ]
--- pages/views.py
+++ pages/views.py
@@ -15,3 +15,14 @@ class HomePageView(TemplateView):
 
 class AboutusPageView(TemplateView):
     template_name = 'pages/aboutus.html'
+
+class WishListView(TemplateView):
+    template_name = 'pages/wishlist.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            user = self.request.user
+            context['favorites'] = user.favorites.all()
+        else:
+            context['favorites'] = []
+        return context
